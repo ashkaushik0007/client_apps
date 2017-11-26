2bb8b5c0d2b2f7e52bb1d629a43457e4c6ad81b2
Aashish Kaushik <aashish.kaushik@BIRLASOFT.COM>
2017-11-26 23:47:13 +0530

    build 1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,5 @@
 export class AppConfig {
-    public readonly apiUrl = 'http://localhost:8080/api';
+    //public readonly apiUrl = 'http://localhost:8080/api';
+    //server
+    public readonly apiUrl = 'http://130.211.227.210/api';
 };
