a314c9ba720b763f325683b0d26df464eb0babb2
Aashish Kaushik <aashish.kaushik@BIRLASOFT.COM>
2018-11-26 21:32:58 +0530

    @like and vigo extration added

--- server/services/AllVideo.service.js
+++ server/services/AllVideo.service.js
@@ -31,73 +31,24 @@ function get(Param) {
         if(reqURL != ''){
             request(reqURL, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
-                    try{
+                    try{  
 
                         const $ = cheerio.load(body);
 
-                        var title = $('title').text();
-
-                        var thumbnail = $("meta[name='og:image']").attr("content");
-
-                        if(typeof thumbnail == 'undefined'){
-                            thumbnail = '';
-                        }
-
-                        var mediaDesc = $('#metadata').children().last().text();
-
-                        var content = body.split('"download_addr":{');
-                        content = content[1].split('}');
-                        content = content[0].split('",');
-                        content = content[0].split('https:');      
-                        finalurl = "https:"+content[1];
-
-                        if(finalurl.indexOf('http') > -1){                        
-                            finalurl = finalurl.replace(/u0026/g, "&");
-                            finalurl = finalurl.replace(/\\/g,'');
+                        if(reqURL.indexOf('tiktok') > -1){ 
+                            //for tiktok
+                            getTiktokParam($,body);
+                        }else if(reqURL.indexOf('vigo') > -1){
+                            //for vigo
+                            getVigoParam($,body);
+                        }else if(reqURL.indexOf('like.video') > -1){
+                            //for Like
+                            getLIKEParam($,body);
                         }else{
-                            // Invalid URL
-                            deferred.reject('Please use valid tiktok | VIGO | LIKE video URL');  
+                            // error
+                            deferred.reject('Please use valid tiktok | VIGO | LIKE video URL'); 
                         }
 
-                        var mp3URL = '';
-
-                        if(body.indexOf('mp3') > -1){
-                            mp3content = body.split('"play_url":{');
-
-                            if(typeof mp3content[1] != 'undefined'){
-                                if(mp3content[1].indexOf('mp3') > -1){
-                                    mp3content = mp3content[1].split('}');
-                                    if(typeof mp3content[0] != 'undefined'){
-                                        mp3content = mp3content[0].split('"],');
-                                        if(typeof mp3content[0] != 'undefined'){
-                                            if(mp3content[0].indexOf('mp3') > -1){
-                                                mp3content = mp3content[1].split('https:');  
-                                                if(typeof mp3content[1] != 'undefined'){
-                                                    mp3content = mp3content[1].split('"');
-                                                    if(typeof mp3content[0] != 'undefined'){
-                                                        if(mp3content[0].indexOf('mp3') > -1){                                                            
-                                                            mp3URL = "https:"+mp3content[0];
-                                                            mp3URL = mp3URL.replace(/u0026/g, "&");
-                                                            mp3URL = mp3URL.replace(/\\/g,'');
-                                                        }                                                        
-                                                    }
-                                                }
-                                            }    
-                                        }
-                                    }
-                                } 
-                            }
-                        }
-
-                        var finaljson = {
-                            "DownloadURL" : finalurl,
-                            "ThumbnailURL" : thumbnail,
-                            "Title" :title,
-                            "MediaDesc":mediaDesc,
-                            "Mp3URL": mp3URL
-                        }
-    
-                        deferred.resolve(finaljson);
                     }catch(error) {
                         // Run time error
                         deferred.reject('Something went wrong, please try again later'); 
@@ -114,7 +65,147 @@ function get(Param) {
             deferred.reject('Please use valid tiktok | VIGO | LIKE video URL');
         }
     }
+
+    function getTiktokParam($,body){
+
+        var title = $('title').text();
+
+        var thumbnail = $("meta[name='og:image']").attr("content");
+
+        if(typeof thumbnail == 'undefined'){
+            thumbnail = '';
+        }
+
+        var mediaDesc = $('#metadata').children().last().text();
+
+        var mp3URL = '';
+
+        if(body.indexOf('mp3') > -1){
+            mp3content = body.split('"play_url":{');
+
+            if(typeof mp3content[1] != 'undefined'){
+                if(mp3content[1].indexOf('mp3') > -1){
+                    mp3content = mp3content[1].split('}');
+                    if(typeof mp3content[0] != 'undefined'){
+                        mp3content = mp3content[0].split('"],');
+                        if(typeof mp3content[0] != 'undefined'){
+                            if(mp3content[0].indexOf('mp3') > -1){
+                                mp3content = mp3content[1].split('https:');  
+                                if(typeof mp3content[1] != 'undefined'){
+                                    mp3content = mp3content[1].split('"');
+                                    if(typeof mp3content[0] != 'undefined'){
+                                        if(mp3content[0].indexOf('mp3') > -1){                                                            
+                                            mp3URL = "https:"+mp3content[0];
+                                            mp3URL = mp3URL.replace(/u0026/g, "&");
+                                            mp3URL = mp3URL.replace(/\\/g,'');
+                                        }                                                        
+                                    }
+                                }
+                            }    
+                        }
+                    }
+                } 
+            }
+        }
+
+        var content = body.split('"download_addr":{');
+        content = content[1].split('}');
+        content = content[0].split('",');
+        content = content[0].split('https:');      
+        finalurl = "https:"+content[1];
+
+        if(finalurl.indexOf('http') > -1){                        
+            finalurl = finalurl.replace(/u0026/g, "&");
+            finalurl = finalurl.replace(/\\/g,'');
+            
+        }else{
+            // Invalid URL
+            deferred.reject('Please use valid tiktok | VIGO | LIKE video URL');  
+        }     
+
+        var PostJson = {
+            "DownloadURL" : finalurl,
+            "ThumbnailURL" : thumbnail,
+            "Title" :title,
+            "MediaDesc":mediaDesc,
+            "Mp3URL": mp3URL
+        }
+
+        //callback
+        deferred.resolve(PostJson);
+    }
+
+    function getVigoParam($,body){
+
+        var title = $('title').text();
+
+        var thumbnail = $("meta[name='og:image']").attr("content");
+
+        if(typeof thumbnail == 'undefined'){
+            thumbnail = '';
+        }
+
+        var mediaDesc = $('.nickname').text();
+
+        var mp3URL = '';
+
+        var finalurl = $('.player-container').attr("data-src");
+
+        if(finalurl.indexOf('http') > -1){                        
+            finalurl = finalurl.replace(/u0026/g, "&");
+            finalurl = finalurl.replace(/\\/g,'');            
+        }else{
+            // Invalid URL
+            deferred.reject('Please use valid tiktok | VIGO | LIKE video URL');  
+        }     
+
+        var PostJson = {
+            "DownloadURL" : finalurl,
+            "ThumbnailURL" : thumbnail,
+            "Title" :title,
+            "MediaDesc":mediaDesc,
+            "Mp3URL": mp3URL
+        }
+
+        //callback
+        deferred.resolve(PostJson);
+    }
+
+    function getLIKEParam($,body){
+
+        var title = $('title').text();
+
+        var thumbnail = $("meta[name='twitter:image']").attr("content");
+
+        if(typeof thumbnail == 'undefined'){
+            thumbnail = '';
+        }
+
+        var mediaDesc = '';
+
+        var mp3URL = '';
+
+        var finalurl = $('#videoPlayer').attr("src");
+
+        if(finalurl.indexOf('http') > -1){                        
+            finalurl = finalurl.replace(/u0026/g, "&");
+            finalurl = finalurl.replace(/\\/g,'');            
+        }else{
+            // Invalid URL
+            deferred.reject('Please use valid tiktok | VIGO | LIKE video URL');  
+        }     
+
+        var PostJson = {
+            "DownloadURL" : finalurl,
+            "ThumbnailURL" : thumbnail,
+            "Title" :title,
+            "MediaDesc":mediaDesc,
+            "Mp3URL": mp3URL
+        }
+
+        //callback
+        deferred.resolve(PostJson);
+    }
     
     return deferred.promise;
-}
- 
+} 
