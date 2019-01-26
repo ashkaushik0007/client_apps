9770de3d524085ffb37e38f5e02404c631cccc9c
Aashish Kaushik <aashish.kaushik@BIRLASOFT.COM>
2019-01-26 13:44:19 +0530

    @Release

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 export class AppConfig {
     //server
-    public readonly apiUrl = 'http://34.73.76.89:8383/api';
+    public readonly apiUrl = 'http://sonalitrader.com/api';
     
     //public readonly apiUrl = 'http://localhost:8383/api';
 }; 
